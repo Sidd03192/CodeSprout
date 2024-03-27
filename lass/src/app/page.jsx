421e5cc364482e6bfcff55e429ef4dce502db2e2
Lucas Frazao <94771586+Lucas-Frazao@users.noreply.github.com>
2024-03-27 12:51:08 -0500

    Updates

--- lass/src/app/page.jsx
+++ lass/src/app/page.jsx
@@ -7,13 +7,20 @@ export default function Landing() {
     <section className="w-full inline-block text-center ">
 
       <h1 className="head_text text-center">
-        <span className=" mr-4 inline-block">Code Sprout</span>  {/* Added margin-right */}
+        <span className="mr-4 inline-block">Code Sprout</span>{" "}
+        {/* Added margin-right */}
         <br className="max-md:hidden" />
-        <span className="  green_gradient inline-block ">Cultivate Your Coding Potential</span>
+        <span className="green_gradient fix-cut-off inline-block">
+          Cultivate Your Coding Potential
+        </span>
       </h1>
+<<<<<<< Updated upstream:lass/src/app/page.jsx
       <p className="desc">
         LASS is a online ed-tech platform which helps students learn to code at lightning speeds.
       </p>
+=======
+      {/* ... rest of your component ... */}
+>>>>>>> Stashed changes:lass/src/app/page.tsx
     </section>
   );
 }
