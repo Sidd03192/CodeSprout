370e49d43a18302a3f53d52a3a53e7d6d4a8a4e9
Sidd03192 <135279087+Sidd03192@users.noreply.github.com>
2024-04-01 12:33:17 -0500

    asdf

--- lass/my-app/src/app/page.jsx
+++ lass/my-app/src/app/page.jsx
@@ -1,23 +1,21 @@
 import Image from "next/image";
 
-
 export default function Landing() {
   return (
-    <section className=" w-full inline-block text-center ">
-
-      <h1 className="head_text text-center">
-        <span className="mr-4 inline-block">Code Sprout</span>{" "}
-        {/* Added margin-right */}
-        <br className="max-md:hidden" />
-        <span className="green_gradient fix-cut-off inline-block">
-          Cultivate Your Coding Potential
-        </span>
-      </h1>
-      <p className="desc font-poppins" style={{ margin: '0 auto' }}>
-  LASS is a online ed-tech platform which helps students learn to code at lightning speeds.
-</p>
-      
-    </section>
+    <div className="h-screen w-screen dark:bg-dot-white/[0.2] bg-dot-black/[0.2]">
+      <section className="w-full inline-block text-center">
+        <h1 className="head_text text-center">
+          <span className="mr-4 inline-block text-white">Code Sprout</span>{" "}
+          {/* Added margin-right */}
+          <br className="max-md:hidden" />
+          <span className="green_gradient fix-cut-off inline-block">
+            Cultivate Your Coding Potential
+          </span>
+        </h1>
+        <p className="desc font-poppins" style={{ margin: '0 auto' }}>
+          LASS is a online ed-tech platform which helps students learn to code at lightning speeds.
+        </p>
+      </section>
+    </div>
   );
 }
-
