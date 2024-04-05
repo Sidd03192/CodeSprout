ac0501dcd999e454d018c8ee6c05a95bc67bb6f2
Lucas Frazao <94771586+Lucas-Frazao@users.noreply.github.com>
2024-04-05 12:41:32 -0500

    Updates

--- lass/my-app/src/app/layout.jsx
+++ lass/my-app/src/app/layout.jsx
@@ -1,37 +1,33 @@
-
 import '@/styles/globals.css'
+import { NextUIProvider } from "@nextui-org/react";
 
-import {NextUIProvider} from "@nextui-org/react";
+export const metadata = {
+    title: "Code Sprout",
+    description: "Cultivate Your Coding"
+};
 
-
-
-export const metadata ={
-    title:"Code Sprout",
-    description:"Cultivate Your Coding"
-}
-const RootLayout = ({children}) => {
-  return (
-        
-    
-
-    <html>
-        <body>
-
-
-          
-            <div className="main">
-                <div className="gradient"/>
-            </div>
-            <main className='app'>
-                {/* Gets Children from the page file. ( displays whatever u want) */}
-                {children}
-
-            </main>
-        </body>
-      
-    </html>
-    
-  )
-}
+const RootLayout = ({ children }) => {
+    return (
+        <html>
+            <body>
+                <div className="taskbar" style={{ backgroundColor: "dark-gray", display: "flex", justifyContent: "space-between", height: "2.5rem" }}>
+                    <div className="taskbar-left">
+                        <strong>Taskbar</strong>
+                    </div>
+                    <div className="taskbar-right" style={{ marginRight: "1rem" }}>
+                        <button>Login</button>
+                    </div>
+                </div>
+                <div className="main">
+                    <div className="gradient" />
+                </div>
+                <main className="app">
+                    {/* Gets Children from the page file. ( displays whatever u want) */}
+                    {children}
+                </main>
+            </body>
+        </html>
+    );
+};
 
 export default RootLayout;
