7b817b9b48730693c525f0765c3190cfe5824a06
Coding With Shawn <codingwithshawn@hash.fyi>
2024-04-09 12:39:36 -0500

    fixed quotes

--- lass/my-app/src/components/testemonials.jsx
+++ lass/my-app/src/components/testemonials.jsx
@@ -18,31 +18,31 @@ export function InfiniteMovingCardsDemo() {
 const testimonials = [
   {
     quote:
-      "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
-    name: "Charles Dickens",
-    title: "A Tale of Two Cities",
+      "I really like the UI. It's so easy to work with and extremely simple compared to competitors.",
+    name: "Rayvan Alexander Watson Jr.",
+    title: "CS Teacher @ Bridgeland High",
   },
   {
     quote:
-      "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
-    name: "William Shakespeare",
-    title: "Hamlet",
+      "You either lock in or cock in. Either way, use Code Sprout.",
+    name: "Adarsh Hegde",
+    title: "CS Student @ Bridgeland High",
   },
   {
-    quote: "All that we see or seem is but a dream within a dream.",
-    name: "Edgar Allan Poe",
-    title: "A Dream Within a Dream",
-  },
-  {
-    quote:
-      "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
-    name: "Jane Austen",
-    title: "Pride and Prejudice",
+    quote: "This website changed the way I give assignments. It is the first truly functional platform that I can safely give coding tests on, and it has been integral for me to help students understand rather than just learn AI.",
+    name: "Nikolaos V. Tsekos    ",
+    title: "AI Professor @ University of Houston",
   },
   {
     quote:
-      "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
-    name: "Herman Melville",
-    title: "Moby-Dick",
+      "LOVE this website. It's easy to use, has all the features you could want, and students find it hassle-free. Highly recommend!",
+    name: "Andrea Arpaci-Dusseau",
+    title: "CS Professor @ UW Madison",
+  },
+  {
+    quote:
+      "I find this a great testing platform. It's simple and reliable.",
+    name: "Stacey Armstrong",
+    title: "CS Teacher at Cypress Woods High",
   },
 ];
