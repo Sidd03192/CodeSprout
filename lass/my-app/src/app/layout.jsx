f452e093e69bb99506b3d04a2766aa4f535376b9
Adarsh hegde <112007944+GithubProgr@users.noreply.github.com>
2024-04-24 11:47:51 -0500

    Navbar
    Navbar pushed

--- lass/my-app/src/app/layout.jsx
+++ lass/my-app/src/app/layout.jsx
@@ -1,5 +1,7 @@
 import '@/styles/globals.css'
-import { NextUIProvider } from "@nextui-org/react";
+import Navbar from "@/components/Navbar.jsx"
+import React from "react";
+import "@/components/Nav.css";
 
 export const metadata = {
     title: "Code Sprout",
@@ -9,20 +11,22 @@ export const metadata = {
 const RootLayout = ({ children }) => {
     return (
         <html>
+            
             <body>
-                <div className="taskbar" style={{ backgroundColor: "dark-gray", display: "flex", justifyContent: "space-between", height: "2.5rem" }}>
-                    <div className="taskbar-left">
-                        <strong>Taskbar</strong>
-                    </div>
-                    <div className="taskbar-right" style={{ marginRight: "1rem" }}>
-                        <button>Login</button>
-                    </div>
-                </div>
+            
+            <Navbar/>
+            
+                
+                
+            
+
                 <div className="main">
                     <div className="gradient" />
                 </div>
+
+
                 <main className="app">
-                    {/* Gets Children from the page file. ( displays whatever u want) */}
+                    {}
                     {children}
                 </main>
             </body>
--- lass/my-app/src/app/page.jsx
+++ lass/my-app/src/app/page.jsx
@@ -1,52 +1,21 @@
-"use client"
-
 import Image from "next/image";
-import WordOfTheDay from '../components/WordOfTheDay.client'; // Import the new component
-import { TypewriterEffectSmooth } from "../components/typewriter";
-import { InfiniteMovingCardsDemo } from "../components/testemonials";
-import { Button } from "@nextui-org/react";
+
 export default function Landing() {
-  const words = [
-    {
-      text: "Get",
-      className: "text-white dark:text-white",
-    },
-    {
-      text: "Started",
-      className: "text-white dark:text-white",
-    },
-    {
-      text: "Now",
-      className: "text-blue-500 dark:text-blue-500",
-    },
-  ];
   return (
     <div className="h-screen w-screen dark:bg-dot-white/[0.2] bg-dot-black/[0.2]">
-      <section className="w-full inline-block text-center mb-100">
+      <section className="w-full inline-block text-center">
         <h1 className="head_text text-center">
           <span className="mr-4 inline-block text-white">Code Sprout</span>{" "}
+          {/* Added margin-right */}
           <br className="max-md:hidden" />
           <span className="green_gradient fix-cut-off inline-block">
-            Cultivate Your Coding Potential
+            Cultivate Your Coding
           </span>
         </h1>
         <p className="desc font-poppins" style={{ margin: '0 auto' }}>
           LASS is a online ed-tech platform which helps students learn to code at lightning speeds.
         </p>
       </section>
-      <div className="text-center flex justify-center items-center w-screen mt-10 text-5xl transform hover:-translate-y-1 transition-all duration-200">
-        <TypewriterEffectSmooth className="typewriter" words={words} />
-      </div>
-      <div className="flex justify-center mb-0">
-      <Button radius="full" className="bg-gradient-to-tr  from-blue-900 via-indigo-600 to-purple-900 text-white shadow-lg mr-10 mb-0">
-      I'm a Student
-    </Button>
-    <Button radius="full" className="bg-gradient-to-tr from-purple-900 via-indigo-600 to-blue-900 text-white shadow-lg ml-4">
-      I'm a Teacher
-    </Button>
-      </div>
-      <WordOfTheDay /> {/* Include the new component */}
-      <InfiniteMovingCardsDemo/>
     </div>
   );
 }
