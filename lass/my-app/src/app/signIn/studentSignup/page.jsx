a8b57e4b3001e4fc3e54ecde867086c712702d97
Sidd03192 <135279087+Sidd03192@users.noreply.github.com>
2024-04-24 12:05:06 -0500

    asdf

--- lass/my-app/src/app/signIn/studentSignup/page.jsx
+++ lass/my-app/src/app/signIn/studentSignup/page.jsx
@@ -187,26 +187,6 @@ const handleSignIn = async (event) => {
     }
 
   }
-};
-
-
-const createUser = async () => {
-  console.log('userData:', userData);
-
-try {
-  const docRef = await addDoc(collection(db, 'users'), {
-    ...userData,
-    skool:school,
-  });
-  console.log('Document written with ID:', docRef.id);
-} catch (error) {
-  console.error('Error adding document:', error);
-}
-
-} 
-
-
-
   return (
     <div className="background-image">
 
@@ -441,6 +421,12 @@ try {
     </div>
 
   )
+
+
+};
+
+
+
 
 
 
