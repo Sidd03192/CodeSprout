f9e3c448a78bfcc53a37f241ccb54c619a0b82bb
School-Lucas-Frazao <94771586+Lucas-Frazao@users.noreply.github.com>
2024-05-08 12:36:50 -0500

    Creates StudentPage

--- lass/my-app/src/app/studentPage/layout.jsx
+++ lass/my-app/src/app/studentPage/layout.jsx
@@ -1,21 +1,25 @@
 'use client'
 
+
 import React from 'react';
-import { Route, Switch} from 'react-router-dom';
+import './Sidebar.css';  // Assuming your styles will be in Sidebar.css
 
-const Layout = ({ children }) => {
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <div className="username-section">
+                Username
+            </div>
+            <div className="classrooms">
+                <h2>Classrooms:</h2>
+                <ul>
+                    <li>Mr. Watson</li>
+                    <li>Mr. Garcia</li>
+                </ul>
+            </div>
+            <button className="add-button">+</button>
+        </div>
+    );
+}
 
-
-  return (
-    <html>
-      <body >
-
-
-        {children}
-
-      </body>
-    </html>
-  );
-};
-
-export default Layout;
+export default Sidebar;
