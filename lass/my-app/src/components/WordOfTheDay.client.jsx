89d6934a2be03ac60fd10c1aca29e97d1d20bec7
Coding With Shawn <codingwithshawn@hash.fyi>
2024-05-09 12:33:30 -0500

    Update WordOfTheDay.client.jsx

--- lass/my-app/src/components/WordOfTheDay.client.jsx
+++ lass/my-app/src/components/WordOfTheDay.client.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Confetti from 'react-confetti';
 
 // This could be fetched from a server
