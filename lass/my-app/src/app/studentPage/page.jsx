86847e5c62c67286d5ac0e202f90260080f3db6c
Coding With Shawn <codingwithshawn@hash.fyi>
2024-05-15 12:15:37 -0500

    asd

--- lass/my-app/src/app/studentPage/page.jsx
+++ lass/my-app/src/app/studentPage/page.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Page = () => {
     return (
         <div>
-            <h1>Student Page</h1><h1>Student Page</h1><h1>Student Page</h1><h1>Student Page</h1><h1>Student Page</h1><h1>Student Page</h1><h1>Student Page</h1><h1>Student Page</h1><h1>Student Page</h1><h1>Student Page</h1><h1>Student Page</h1><h1>Student Page</h1><h1>Student Page</h1><h1>Student Page</h1><h1>Student Page</h1><h1>Student Page</h1><h1>Student Page</h1><h1>Student Page</h1><h1>Student Page</h1><h1>Student Page</h1><h1>Student Page</h1><h1>Student Page</h1><h1>Student Page</h1><h1>Student Page</h1><h1>Student Page</h1><h1>Student Page</h1><h1>Student Page</h1><h1>Student Page</h1><h1>Student Page</h1><h1>Student Page</h1><h1>Student Page</h1><h1>Student Page</h1><h1>Student Page</h1><h1>Student Page</h1><h1>Student Page</h1>
+            <h1>Student Page</h1>
             {/* Add your content here */}
         </div>
     );
